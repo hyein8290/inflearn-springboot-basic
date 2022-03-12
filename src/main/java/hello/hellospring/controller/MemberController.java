aafe6aff74004443271fea066c7446eb9e3ff8f1
hyein8290 <hyein8290@gmail.com>
2022-03-12 23:49:11 +0900

    컴포넌트 스캔과 자동 의존관계 설정

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -4,6 +4,8 @@ import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
+//@Component를 포함하는 애노테이션은 스프링 빈으로 자동 등록된다.
+//  -> @Controller, @Service, @Repository
 @Controller
 public class MemberController {
 
